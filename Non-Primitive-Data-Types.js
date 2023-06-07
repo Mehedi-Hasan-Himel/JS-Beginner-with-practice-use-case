16a157ee12449ec22993ebf0368cf88096377543
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-07 07:55:42 +0600

    NON - Primitive Data Types

---
+++ Non-Primitive-Data-Types.js
@@ -0,0 +1,27 @@
+/* 
+NON - Primitive Data Types
+
+Data Type vs Descriptions.
+1. Object: Represents instance through which we can access members.
+2. Array: Represents group of similar values.
+3. RegExp: Represents regular expression.
+*/
+
+// Object
+// 1. Object: Represents instance through which we can access members.
+
+var A = {
+  Name: "Himel",
+  Age: 30,
+  City: "Gazipur",
+};
+
+console.log(A)
+
+
+
+// Array
+// 2. Array: Represents group of similar values.
+var B = ["Moulvibazar", "Dhaka", "Gazipur"];
+
+console.log(B)
