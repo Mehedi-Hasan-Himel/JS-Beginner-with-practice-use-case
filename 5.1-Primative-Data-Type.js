64653892cfec81e1c08d6e1719b87cc20cb5b82a
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-07 08:04:21 +0600

    5.1-Primative-Data-Type.js

---
+++ 5.1-Primative-Data-Type.js
@@ -0,0 +1,48 @@
+/* 
+JavaScript Data Types.
+JavaScript provides different types to hold different types of values. There are two types of data types in JavaScript.
+1. Primitive data type
+2. Non-primitive (reference) data type
+*/
+
+// Primitive Data Types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol
+
+// Data Type vs Descriptions.
+// 1. String: Represents sequence of characters e.g. "hello".
+// 2. Number: Represents numeric values e.g. 100.
+// 3. Boolean: Represents boolean value either false or true.
+// 4. Undefined: Represents undefined value. 
+// 5. Null: Represents null i.e. no value at all.
+// 6. Symbol: Represents unique instance of primitive data type.
+
+// String Example
+var myName = "John Doe";
+console.log(myName);
+
+// Number Example
+var myNumber = 100;
+console.log(myNumber);
+
+// Boolean Example
+var isTrue = true;
+console.log(isTrue);
+
+// Undefined Example
+var myUndefined;
+console.log(myUndefined);
+
+// Null Example
+var myNull = null;
+console.log(myNull);
+
+// Symbol Example
+var mySymbol = Symbol("mySymbol");
+
+
+
