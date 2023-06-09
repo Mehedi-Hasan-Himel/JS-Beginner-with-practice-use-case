1673438309fa0cdd4e03d9a96444779e713e273c
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-09 19:37:50 +0600

    10-loop-break-continue

---
+++ 10-loop-break-continue.js
@@ -0,0 +1,16 @@
+/* var i;
+for (i = 0; i < 10; i++) {
+  if (i === 5 || i === 8) {
+    continue;
+  }
+  document.write(i + " - " + "<button>Submit</button> <br> <br>");
+}
+ */
+
+var i;
+for (i = 0; i < 10; i++) {
+  if (i === 3) {
+    break;
+  }
+  document.write(i + " - " + "<button>Submit</button> <br> <br>");
+}
