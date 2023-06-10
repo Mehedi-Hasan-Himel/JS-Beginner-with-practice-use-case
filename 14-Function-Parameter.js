43a8c6720657e90daff8dbe749d77e756c5dac9e
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-10 22:14:44 +0600

    function parameter

---
+++ 14-Function-Parameter.js
@@ -0,0 +1,19 @@
+/* function addTwoNumbers(num1, num2) {
+  var num3 = num1 + num2;
+  document.write(num3 + "<br>");
+}
+
+
+addTwoNumbers(10, 20);
+addTwoNumbers(100, 200);
+addTwoNumbers(1000, 2000); */
+
+
+function WriteMyName(name) {
+  document.write(name + "<br>");
+}
+
+WriteMyName("Ali");
+WriteMyName("Reza");
+
+   
