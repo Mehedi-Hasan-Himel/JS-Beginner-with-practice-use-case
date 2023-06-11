0aca5df15a57e2fb1f917f01512a502c2f004980
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-11 09:17:51 +0600

    18 For Loop Over Array

---
+++ 18-For-Loop-Over-Array.js
@@ -0,0 +1,16 @@
+var catagory = [
+  "Allium",
+  "Bevarages",
+  "Dairy Bread & Eggs",
+  "Fruits & Vegetables",
+   "Meat & Fish",
+   "Health & Beauty",
+   "Household",
+   "Kitchen & Dining",
+   "Snacks & Desserts",
+];
+
+var i;
+for (i = 0; i < catagory.length; i++){
+   document.write(catagory[i] + "<br>");
+}
