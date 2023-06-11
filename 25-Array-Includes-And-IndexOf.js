4024d5138fa1bfa1c7a862b5c62725ff411f5a7a
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-11 12:07:58 +0600

    cod

---
+++ 25-Array-Includes-And-IndexOf.js
@@ -0,0 +1,6 @@
+var numArray = [30, 32, 40, 55, 60, 80, 90,100];
+
+// var result = numArray.includes(100); 
+var result = numArray.indexOf(1000);
+
+document.write(result); // 55
---
+++ 26-array-pop-push-reverse.js
@@ -0,0 +1,15 @@
+var numArray = [30, 32, 40, 55, 60, 80, 90];
+
+// Reverse the array
+// var result = numArray.reverse();
+
+// document.write(result);
+
+// push the array
+// var result = numArray.push(100);
+// document.write(numArray);
+
+// pop the array
+var result = numArray.pop();
+
+document.write(numArray);
