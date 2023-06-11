ffdf10b795e0dd159426671cd9bfb22dcca919a8
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-11 12:39:41 +0600

    string methods

---
+++ 29-String-Methods.js
@@ -0,0 +1,10 @@
+var Country = `Bangladesh`;
+var Country2 = `India`;
+
+// document.write(Country.charAt(5));
+
+console.log(Country.concat(Country2));
+
+console.log(Country.indexOf(`a`));
+
+console.log(Country.lastIndexOf(`a`));
