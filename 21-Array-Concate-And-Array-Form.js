08484701f03399e2250b781a2d7de9d2294205fd
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-11 09:35:27 +0600

    21 Array Concate And Array Form

---
+++ 21-Array-Concate-And-Array-Form.js
@@ -0,0 +1,13 @@
+var arr1 = ["A", "B", "C"];
+var arr2 = ["D", "E", "F"];
+
+var arr3 = arr2.concat(arr1); // ["A", "B", "C", "D", "E", "F"]
+
+
+// document.write(arr3);
+
+
+var name = "Code with Himel"
+var nameArray = Array.from(name);
+
+document.write(nameArray); // ["C", "o", "d", "e", " ", "w", "i", "t", "h", " ", "H", "i", "m", "e", "l"]
