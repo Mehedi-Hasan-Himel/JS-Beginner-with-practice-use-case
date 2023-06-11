de221a3333440826824392864f5610cec35872ea
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-11 08:56:11 +0600

    object js

---
+++ 15-function-return.js
@@ -0,0 +1,7 @@
+function addTwoNumbers(a, b) {
+  return a + b;
+}
+
+
+var result = addTwoNumbers(10, 20);
+document.write(result + "<br>");
---
+++ 16-object.js
@@ -0,0 +1,14 @@
+var MyPc = {
+  brand: "Apple",
+  screen: "13.3 inch",
+  isLaptop: true,
+  model: "MacBook Air 2017",
+  ram: "8GB",
+  storage: "128GB",
+  isSSd: true,
+  cpu: "Intel Core i5",
+  color: "Silver",
+  price: 10000000,
+};
+
+document.write(MyPc.isSSd);
