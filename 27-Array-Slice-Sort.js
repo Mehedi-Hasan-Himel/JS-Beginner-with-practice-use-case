7635e20dee52ccfd81b177fba01a3b38f97b7d25
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-06-11 12:21:26 +0600

    27-Array-Slice-Sort.js

---
+++ 27-Array-Slice-Sort.js
@@ -0,0 +1,16 @@
+var numArray = [30, 5, 32, 40, 445, 55, 60, 777, 80, 90];
+
+//  sort array
+// var result = numArray.sort();
+// document.write(result);
+
+//  sort array reverse
+// var resultReverse = numArray.sort().reverse();
+// document.write(resultReverse);
+
+
+var resultSlice = numArray.slice(2, 5);
+document.write(resultSlice);
+
+
+// this file name : 27-Array-Slice-Sort.js
